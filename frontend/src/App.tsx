8d5002741080a80edaa0e202e40717b82a7043fb
Wanda Young <wyoung@sandiego.edu>
2024-11-16 21:17:13 -0800

    initial commit with dependencies for frontend and backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flex items-center mt-10">
+        Welcome to scribbl
+      </div>
+    </div>
+  );
+}
+
+export default App;
